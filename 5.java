3e01d6b698f74d8a4356cc7669ee6f1e4fd235de
anushabollineni <41730220+anushabollineni@users.noreply.github.com>
2018-07-27 10:55:39 +0530

    Create 5.java

---
+++ 5.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n1=1,n2=1,n3=-4;
+	if(n1 >= n2 && n1 >= n3)
+	System.out.println(n1 + " is largest num"); 
+	else if(n2 >= n1 && n2 >= n3)
+	System.out.println(n2 + "n2 is largest num");
+	else 
+	System.out.println(n3 + "n3 is largest num"); // your code goes here
+	}
+}
